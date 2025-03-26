356a0ae01f37190e098902b91cc6941afebb1f2f
John Ndungu <ndungulink@gmail.com>
2025-03-26 06:42:32 +0300

    Created  my models

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class TODO(models.Model):
+class TODOO(models.Model):
+    srno=models.AutoField(primary_key=True,auto_created=True)
+    title=models.CharField(max_length=25)
+    date=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     
-    
+#make the migrations to create the database. -- py manage.py makemigrations todo
+#finalize the migration -- py manage.py migrate
+
